617f4a1b48f2e771808066c40c8c26c30374d72c
yukihirop <te108186@gmail.com>
2019-01-23 21:20:59 +0900

    Add test for jest

---
+++ src/components/__tests__/VueTagEditor.spec.js
@@ -0,0 +1,49 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import VueTagEditor from '../VueTagEditor'
+
+const localVue = createLocalVue()
+
+describe('VueTagEditor', () => {
+  const wrapper = shallowMount(VueTagEditor, {
+    localVue,
+    propsData: {
+      tags: ['javascript', 'ruby']
+    }
+  })
+
+  it('correct name options', () => {
+    expect(wrapper.vm.$options.name).toBe('vue-tag-editor')
+  })
+
+  it('correct default props', () => {
+    expect(wrapper.props().type).toBe('label')
+  })
+
+  it('renders correctly (snapshots)', () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('render the correct markup', () => {
+    expect(wrapper.html()).toContain("<tags-stub tags=\"javascript,ruby\" type=\"label\" eventhub=\"[object Object]\"></tags-stub>")
+    expect(wrapper.html()).toContain("<input placeholder=\"Add tags...\">")
+  })
+
+  it('has a input', () => {
+    expect(wrapper.contains('input')).toBe(true)
+  })
+
+  describe('Cursor up create tag', () => {
+    const input = wrapper.find('input')
+
+    // Can not test update props
+    // https://github.com/vuejs/vue-test-utils/issues/480#issuecomment-374744653
+    describe('input tag do not exists', () => {
+      it('create tag', () => {
+        input.setValue('swift')
+        input.trigger('keydown.up')
+
+        expect(true).toEqual(true)
+      })
+    })
+  })
+})
---
+++ src/components/tags/__tests__/TagLink.spec.js
@@ -0,0 +1,56 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import TagLink from '../TagLink'
+
+const localVue = createLocalVue()
+
+const wrapperEventHub = mount(TagLink, {
+  localVue,
+  propsData: {
+    tagname: 'test'
+  }
+})
+const eventHub = wrapperEventHub.vm
+
+describe('TagLink', () => {
+  const wrapper = mount(TagLink, {
+    localVue,
+    propsData: {
+      tagname: 'test',
+      eventHub: eventHub
+    }
+  })
+
+  it('correct name options', () => {
+    expect(wrapper.vm.$options.name).toBe('tag-link')
+  })
+
+  it('renders correctly (snapshots)', () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('renders the correct markup', () => {
+    expect(wrapper.html()).toContain("<span><span><a><span>test</span></a></span>")
+    expect(wrapper.html()).toContain("x")
+  })
+
+  it('has a delete button', () => {
+    expect(wrapper.contains('button')).toBe(true)
+  })
+
+  it('delete button click should call emitDeleteTag', () => {
+    const button = wrapper.find('button')
+    button.trigger('click')
+
+    expect(wrapper.emitted('delete-tag')).toBeTruthy()
+    expect(wrapper.emitted('delete-tag').length).toBe(1)
+  })
+
+  it('tag-click button click should call emitTagClick', () => {
+    const anchor = wrapper.find({ ref: 'tagname'})
+    anchor.trigger('click')
+
+    expect(wrapperEventHub.emitted('tag-click')).toBeTruthy()
+    expect(wrapperEventHub.emitted('tag-click').length).toBe(1)
+    expect(wrapperEventHub.emitted('tag-click')[0]).toEqual(['test'])
+  })
+})
