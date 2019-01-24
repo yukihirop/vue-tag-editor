767cd7931496a8b74a301f49ee4f7f114ebf7f91
yukihirop <te108186@gmail.com>
2019-01-24 14:21:36 +0900

    Update demo after Implement VueTagEditorBulma

--- demo/App.vue
+++ demo/App.vue
@@ -1,20 +1,49 @@
 <template>
   <span>
-    <tag-editor
-      :tags='tagLabels'
-      :type="'label'"
-      @handler-after-input-tag='handlerAfterInputTag'
-      @handler-after-delete-tag='handlerAfterDeleteTag'
-    ></tag-editor>
-    <br/>
-    <!-- handler-after-click-tag is effective only when type === 'link' -->
-    <tag-editor
-      :tags='tagLinks'
-      :type="'link'"
-      @handler-after-click-tag='handlerAfterClickTag'
-      @handler-after-input-tag='handlerAfterInputTag'
-      @handler-after-delete-tag='handlerAfterDeleteTag'
-    ></tag-editor>
+    <div>
+      <h2>None Style</h2>
+      <span>
+        <span>
+          <tag-editor
+            :tags='tagLabels'
+            :type="'label'"
+            @handler-after-input-tag='handlerAfterInputTag'
+            @handler-after-delete-tag='handlerAfterDeleteTag'
+          ></tag-editor>
+          <br/>
+          <!-- handler-after-click-tag is effective only when type === 'link' -->
+          <tag-editor
+            :tags='tagLinks'
+            :type="'link'"
+            @handler-after-click-tag='handlerAfterClickTag'
+            @handler-after-input-tag='handlerAfterInputTag'
+            @handler-after-delete-tag='handlerAfterDeleteTag'
+          ></tag-editor>
+        </span>
+      </span>
+    </div>
+    <div>
+      <h2>Bulma</h2>
+      <span>
+        <span>
+          <tag-editor-bulma
+            :tags='tagLabelsBulma'
+            :type="'label'"
+            @handler-after-input-tag='handlerAfterInputTag'
+            @handler-after-delete-tag='handlerAfterDeleteTag'
+          ></tag-editor-bulma>
+          <br/>
+          <!-- handler-after-click-tag is effective only when type === 'link' -->
+          <tag-editor-bulma
+            :tags='tagLinksBulma'
+            :type="'link'"
+            @handler-after-click-tag='handlerAfterClickTag'
+            @handler-after-input-tag='handlerAfterInputTag'
+            @handler-after-delete-tag='handlerAfterDeleteTag'
+          ></tag-editor-bulma>
+        </span>
+      </span>
+    </div>
   </span>
 </template>
 
@@ -23,7 +52,9 @@ export default {
   data(){
     return {
       tagLabels: ['javascript', 'ruby'],
-      tagLinks:  ['javascript', 'ruby']
+      tagLinks:  ['javascript', 'ruby'],
+      tagLabelsBulma: ['javascript', 'ruby'],
+      tagLinksBulma:  ['javascript', 'ruby']
     }
   },
   methods: {
